66631310056be75209f56147751d4aa34f6e82a5
Susan Li <suzyn.one@gmail.com>
2017-05-27 04:01:29 -0400

    asdfas

--- server.js
+++ server.js
@@ -6,19 +6,24 @@ app.set("port", process.env.PORT || 8888);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
-})
+});
 
 app.post("/messenger", (req, res) => {
   console.log("post: message");
   console.log("request:" + req);
-  request("https://senderbot.herokuapp.com/react~:love:~" + req.messageID);
+  request(
+    "https://senderbot.herokuapp.com/react~:love:~" + req.message.messageID, function(err, response, body) {
+      console.error(err);
+      console.log('body: ', body);
+    }
+  );
   res.send(req);
 });
 
 app.post("/history", (req, res) => {
   console.log("post: history");
   res.send(req);
-})
+});
 
 app.listen(app.get("port"), function() {
   console.log("running on port ", app.get("port"));
