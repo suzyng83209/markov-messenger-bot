1f7dff74ebf4dc51796bded01578748f22718d92
Susan Li <suzyn.one@gmail.com>
2017-05-27 03:22:58 -0400

    ok should work???

--- MessengerListener.js
+++ MessengerListener.js
@@ -12,20 +12,6 @@ login({ email: userInfo.USERNAME, password: userInfo.PASSWORD }, (err, api) => {
       if (err) console.error(err);
     });
 
-    api.getThreadHistory(message.threadID, 50, timestamp = undefined, (err, history) => {
-      if (err) return console.error(err);
-
-      request.post(
-        { url: "localhost:8888/history", formData: history },
-        function callback(err, httpResponse, body) {
-          if (err) {
-            return console.error("failed", err);
-          }
-          console.log(body);
-        }
-      );
-    });
-
     request.post(
       { url: "localhost:8888/messenger", formData: message },
       function callback(err, httpResponse, body) {
