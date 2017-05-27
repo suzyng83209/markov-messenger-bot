d3f5f3e4114229c3788b18544a824a99e7fe351f
Susan Li <suzyn.one@gmail.com>
2017-05-26 23:41:31 -0400

    making it message dependent not event dependent

--- EchoBot.js
+++ EchoBot.js
@@ -4,49 +4,46 @@ const userInfo = require("./userInfo.js");
 login({ email: userInfo.USERNAME, password: userInfo.PASSWORD }, (err, api) => {
   if (err) return console.error(err);
 
-  let echoState = 'INITIAL';
+  let echoState = "INITIAL";
 
-  api.listen((err, event) => {
+  api.listen((err, message) => {
     if (err) return console.error(err);
 
-    api.markAsRead(event.threadID, (err) => {
+    api.markAsRead(message.threadID, err => {
       if (err) console.error(err);
-    })
+    });
 
-    switch(event.type) {
-      case "message":
-        switch(echoState) {
-          case 'ON':
-            if (event.body === '/stop') {
-              api.sendMessage("Goodbye! Text '/startEcho' to resume.", event.threadID);
-              echoState = 'OFF';
-            } else {
-              api.sendMessage(event.body, event.threadID);
-            }
-            break;
-
-          case 'OFF':
-            if (event.body === '/startEcho') {
-              api.sendMessage("EchoBot will restart Echo", event.threadID);
-              echoState = 'ON';
-            }
-            break;
-          
-          case 'INITIAL':
-            api.sendMessage(event.body, event.threadID);
-            api.sendMessage("EchoBot will start echoing conversations. Text '/stop' to stop.", event.threadID);
-            echoState = 'ON';
-            break;
-
-          default:
-            console.log('...');
-
+    switch (echoState) {
+      case "ON":
+        if (message.body === "/stop") {
+          api.sendMessage(
+            "Goodbye! Text '/startEcho' to resume.",
+            message.threadID
+          );
+          echoState = "OFF";
+        } else {
+          api.sendMessage(message.body, message.threadID);
         }
         break;
 
-      case "event":
-        console.log(event);
+      case "OFF":
+        if (message.body === "/startEcho") {
+          api.sendMessage("EchoBot will restart Echo", message.threadID);
+          echoState = "ON";
+        }
         break;
+
+      case "INITIAL":
+        api.sendMessage(message.body, message.threadID);
+        api.sendMessage(
+          "EchoBot will start echoing conversations. Text '/stop' to stop.",
+          message.threadID
+        );
+        echoState = "ON";
+        break;
+
+      default:
+        console.log("...");
     }
   });
 });
