b0cbbc8816afdbda84ee024feae41947ef4a2be4
Susan Li <suzyn.one@gmail.com>
2017-05-27 03:27:39 -0400

    adding second endpoint

--- MessengerListener.js
+++ MessengerListener.js
@@ -12,6 +12,22 @@ login({ email: userInfo.USERNAME, password: userInfo.PASSWORD }, (err, api) => {
       if (err) console.error(err);
     });
 
+    let timestamp = undefined;
+    api.getThreadHistory(message.threadID, 50, timestamp, (err, history) => {
+      if (err) return console.error(err);
+      if (timestamp != undefined) history.pop();
+      timestamp = history[0].timestamp;
+      request.post(
+        { url: "localhost:8888/history", formData: history },
+        function callback(err, httpsResponse, body) {
+          if (err) {
+            return console.error("failed: ", err);
+          }
+          console.log(body);
+        }
+      );
+    });
+
     request.post(
       { url: "localhost:8888/messenger", formData: message },
       function callback(err, httpResponse, body) {
