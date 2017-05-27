cf46d6ddcd98c1c027264e651be2bd5ff8250cc3
Susan Li <suzyn.one@gmail.com>
2017-05-26 23:27:31 -0400

    fsm

--- EchoBot.js
+++ EchoBot.js
@@ -4,7 +4,49 @@ const userInfo = require("./User.js");
 login({ email: userInfo.username, password: userInfo.password }, (err, api) => {
   if (err) return console.error(err);
 
-  api.listen((err, message) => {
-    api.sendMessage(message.body, message.threadID);
+  let echoState = 'INITIAL';
+
+  api.listen((err, event) => {
+    if (err) return console.error(err);
+
+    api.markAsRead(event.threadID, (err) => {
+      if (err) console.error(err);
+    })
+
+    switch(event.type) {
+      case "message":
+        switch(echoState) {
+          case 'ON':
+            if (event.body === '/stop') {
+              api.sendMessage("Goodbye! Text '/startEcho' to resume.", event.threadID);
+              echoState = 'OFF';
+            } else {
+              api.sendMessage(event.body, event.threadID);
+            }
+            break;
+
+          case 'OFF':
+            if (event.body === '/startEcho') {
+              api.sendMessage("EchoBot will restart Echo", event.threadID);
+              echoState = 'ON';
+            }
+            break;
+          
+          case 'INITIAL':
+            api.sendMessage(event.body, event.threadID);
+            api.sendMessage("EchoBot will start echoing conversations. Text '/stop' to stop.", event.threadID);
+            echoState = 'ON';
+            break;
+
+          default:
+            console.log('...');
+
+        }
+        break;
+
+      case "event":
+        console.log(event);
+        break;
+    }
   });
 });
