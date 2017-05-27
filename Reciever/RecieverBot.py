915b86c4e38d53e565aa16b743a7754781006cd9
silastsui <silastsuii@gmail.com>
2017-05-27 17:29:38 -0400

    remove timotut

--- Reciever/RecieverBot.py
+++ Reciever/RecieverBot.py
@@ -43,8 +43,8 @@ class EchoBot(fbchat.Client):
 
             # TODO
             text=self.m.generate()
-            print "markov chain: ", text
-            time.sleep(random.uniform(1, len(text)/50))
+            # print "markov chain: ", text
+            # time.sleep(random.uniform(1, len(text)/50))
             p = {"command": "send","text": text,"id": str(author_id)}
             r = requests.post("https://senderbot.herokuapp.com", json = p)
 
