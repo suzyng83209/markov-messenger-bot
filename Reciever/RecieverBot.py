eb3a6437d13cddab0b208c9833a805f6d74c986e
Jspsun <Jspsun@gmail.com>
2017-05-27 09:47:54 -0400

    whoops

--- Reciever/RecieverBot.py
+++ Reciever/RecieverBot.py
@@ -7,33 +7,36 @@ import random
 
 
 class EchoBot(fbchat.Client):
+    m = MarkovModel()
+    stores ={"sears":"www.sears.ca", "tunnelbear":"www.tunnelbear.com"}
 
     def __init__(self, email, password, debug=True, user_agent=None):
         fbchat.Client.__init__(self, email, password, debug, user_agent)
-        self.m = MarkovModel()
-        self.enabled = False
 
     def on_message(self, mid, author_id, author_name, message, metadata):
         self.markAsDelivered(author_id, mid)  # mark delivered
         self.markAsRead(author_id)  # mark read
 
-        # if str(author_id) == str(self.uid):
-        #
-        #     if message.body == 'Start':
-        #         self.enabled = True
-        #     elif message.body == 'Stop':
-        #         self.enabled = False
-        #         print self.enabled
-
-        # if you are not the author, echo
         domain = "https://senderbot.herokuapp.com/send~"
-        # print self.enabled
-        if str(author_id) != str(self.uid) and self.enabled:
-            # self.send(author_id, message)
-            time.sleep(random.uniform(1, 5))
+        message=message.lower()
+
+
+        if str(author_id) != str(self.uid):
+            # if message == '/shopping' or message.index("shopping")>=0:
+            #     urllib2.urlopen(domain + "www.sears.ca" + "~" + str(author_id))
+            #     return
+            # elif message ==
+
+            for k in self.stores.keys():
+                if k in message:
+                    urllib2.urlopen(domain + self.stores[k] + "~" + str(author_id))
+                    return
+
+
 
             text=self.m.generate()
             text='%20'.join(text.split())
+            time.sleep(random.uniform(1, len(text)/30))
             urllib2.urlopen(domain + text + "~" + str(author_id))
             # urllib2.urlopen("https://senderbot.herokuapp.com/send~LOL~1135881167")
 
