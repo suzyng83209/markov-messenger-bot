7baf54ca4504273345c527319795ffd36417be72
Susan Li <suzyn.one@gmail.com>
2017-05-27 09:39:46 -0400

    why is heroku like this

--- MessengerListener.js
+++ MessengerListener.js
@@ -56,5 +56,5 @@ app.get("/autorespond", (req, res) => {
 });
 
 app.listen(app.get("port"), function() {
-  console.log("React app is running on port ", app.get("port"));
+  console.log("React app is running on port", app.get("port"));
 });
