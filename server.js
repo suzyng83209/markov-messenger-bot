7c3c28fe73dc03075e90d182a52534bf75fb9c2e
Susan Li <suzyn.one@gmail.com>
2017-05-27 03:27:39 -0400

    adding second endpoint

--- server.js
+++ server.js
@@ -8,10 +8,15 @@ app.get("/", (req, res) => {
 })
 
 app.post("/messenger", (req, res) => {
-  console.log("post");
+  console.log("post: message");
   console.log("request:" + req);
   res.send(req);
 });
+
+app.post("/history", (req, res) => {
+  console.log("post: history");
+  res.send(req);
+})
 
 app.listen(app.get("port"), function() {
   console.log("running on port ", app.get("port"));
